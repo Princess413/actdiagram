bb7a4d0dbd487db46fb851a4b60a555be3265143
Princess413 <35474633+Princess413@users.noreply.github.com>
2019-01-23 13:52:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 </head>
 <body>
 <img src="activitydia.jpg">
-  <p></p>
+  <p>This Activity Diagram simply shows the flow process of the application that's in search for a child.
+  User will firstly log in; when loging in if account doesn't exist user needs to sign up in order that the admin can verify account.
+  After, proceed for searching an adoptee and process the adoption.</p>
 </body>
 </html>
